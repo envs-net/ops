a9fdc5c400d554a6c4d580ed7d21b2c06a316f8d
creme <creme@envs.net>
2020-01-04 01:12:24 +0100

    small changes

--- srv/znc/add_znc_user.sh
+++ srv/znc/add_znc_user.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
-[[ "$EUID" -ne "$(id -u znc)" ]] && printf 'Please run as znc!\n' && exit 1
+if [ "$(id -u)" -ne "$(id -u znc)" ]; then
+	printf 'Please run as znc!\n' ; exit 1
+fi
 
 CONF='/srv/znc/.znc/configs/znc.conf'
 PID="$(pgrep -u znc znc)"
 
-if [[ -z "$1" ]]; then
-    echo -e "Usage: $(basename "$0") [username]"
-    exit 1
+if [ -z "$1" ]; then
+    echo -e "Usage: $(basename "$0") [username]" ; exit 1
 fi
 
 if awk '/<User/ {print $2}' "$CONF" | grep -iq "$1"; then
-    echo -e "znc user \"$1\" already exists"
-    exit 1
+    echo -e "znc user \"$1\" already exists" ; exit 1
 fi
 
 sed -e "s/NEWUSER/$1/g" /srv/znc/newuser.conf.template >> "$CONF"
 
-[[ -n "$2" ]] && kill -s HUP "$PID"
+[ -n "$2" ] && kill -s HUP "$PID"
 
-#
 exit 0
