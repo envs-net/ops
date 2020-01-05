114ff933b63c253f9f47e5680106a609adcd2695
creme <creme@envs.net>
2020-01-05 14:03:14 +0000

    update

--- etc/letsencrypt/renewal-hooks/deploy/envs.sh
+++ etc/letsencrypt/renewal-hooks/deploy/envs.sh
@@ -11,6 +11,35 @@ for domain in $RENEWED_DOMAINS; do
 			cat "$RENEWED_LINEAGE/chain.pem" > "$daemon_cert_root/chain.pem"
 			cat "$RENEWED_LINEAGE/fullchain.pem" > "$daemon_cert_root/fullchain.pem"
 			cat /etc/ssl/certs/envs_dhparam.pem > "$daemon_cert_root/envs_dhparam.pem"
+
+			# matrix
+			matrix_dir=/var/lib/lxc/matrix/rootfs/etc/matrix-synapse
+			cp "$daemon_cert_root/privkey.pem" "$matrix_dir"/
+			cp "$daemon_cert_root/chain.pem" "$matrix_dir"/
+			cp "$daemon_cert_root/fullchain.pem" "$matrix_dir"/
+			chmod 600 "$matrix_dir"/*.pem
+			chown 108:0 "$matrix_dir"/*.pem
+			lxc-attach -n matrix -- bash -c "systemctl reload nginx ; systemctl restart matrix-synapse"
+
+			# mail
+			lxc-attach -n mail -- bash -c "systemctl reload nginx postfix dovecot"
+			# mailinglists
+			lxc-attach -n lists -- bash -c "systemctl reload nginx postfix"
+
+			# gitea
+			lxc-attach -n gitea -- bash -c "systemctl reload nginx"
+
+			# searx
+			lxc-attach -n searx -- bash -c "systemctl reload nginx"
+
+			# cryptad
+			lxc-attach -n pad -- bash -c "systemctl reload nginx"
+
+			# tt-rss
+			lxc-attach -n rss -- bash -c "systemctl restart apache2"
+
+			# privatebin
+			lxc-attach -n pb -- bash -c "systemctl restart apache2"
 			;;
 
 		envs.sh)
@@ -20,6 +49,8 @@ for domain in $RENEWED_DOMAINS; do
 			cat "$RENEWED_LINEAGE/chain.pem" > "$daemon_cert_root/chain.pem"
 			cat "$RENEWED_LINEAGE/fullchain.pem" > "$daemon_cert_root/fullchain.pem"
 			cat /etc/ssl/certs/envs_dhparam.pem > "$daemon_cert_root/envs_dhparam.pem"
+			# 0x0 / fiche
+			lxc-attach -n null -- bash -c "systemctl reload nginx"
 			;;
 
 		znc.envs.net)
@@ -30,6 +61,7 @@ for domain in $RENEWED_DOMAINS; do
 			cat /etc/ssl/certs/envs_dhparam.pem >> "$daemon_cert_root/znc.pem"
 			chown znc "$daemon_cert_root/znc.pem"
 			chmod 600 "$daemon_cert_root/znc.pem"
+			systemctl restart znc
 			;;
 
 	esac
