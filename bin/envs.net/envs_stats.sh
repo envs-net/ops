99408b17b3d88921aad055b32f50af37334628c2
creme <creme@envs.net>
2020-05-18 07:14:39 +0000

    execute envs_stats (goaccess) with nice -n19

--- bin/envs.net/envs_stats.sh
+++ bin/envs.net/envs_stats.sh
@@ -8,7 +8,7 @@ test ! -f /var/www/envs.net/stats/ && mkdir -p /var/www/envs.net/stats/
 	zcat /var/log/nginx/other_vhosts_access.*.gz
 	cat /var/log/nginx/other_vhosts_access.log.1
 	cat /var/log/nginx/other_vhosts_access.log
-} | awk '$8=$1$8' | goaccess -a \
+} | awk '$8=$1$8' | /usr/bin/nice -n19 goaccess -a \
 	-o /var/www/envs.net/stats/index.html \
 	--ignore-panel=HOSTS \
 	--ignore-panel=KEYPHRASES \
